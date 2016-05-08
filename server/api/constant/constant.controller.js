05ca03a0d812f022cc1903090d8543cca8792981
Cuong Nguyen <cuong.nguyen@niteco.se>
2016-05-08 14:25:45 +0700

    đâ

--- server/api/constant/constant.controller.js
+++ server/api/constant/constant.controller.js
@@ -9,6 +9,9 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.getMenu = getMenu;
+exports.getCollection = getCollection;
+exports.getAllProduct = getAllProduct;
+exports.getProduct = getProduct;
 
 var _fs = require('fs');
 
@@ -25,11 +28,36 @@ var _environment2 = _interopRequireDefault(_environment);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var ROOT_RESOURCE = _path2.default.normalize(_environment2.default.root + '/server/resources');
-var CONSTANT_FILE = _path2.default.normalize(ROOT_RESOURCE + '/constant/sorentino.json');
+var CONSTANT_FILE = _path2.default.normalize(ROOT_RESOURCE + '/sorentino.json');
+var PRODUCT_FILE = _path2.default.normalize(ROOT_RESOURCE + '/product.json');
+var COLLECTION_FILE = _path2.default.normalize(ROOT_RESOURCE + '/collection.json');
 
-// Gets a list of Things
+var constant = JSON.parse(_fs2.default.readFileSync(CONSTANT_FILE, 'utf8'));
+var product = JSON.parse(_fs2.default.readFileSync(PRODUCT_FILE, 'utf8'));
+var collection = JSON.parse(_fs2.default.readFileSync(COLLECTION_FILE, 'utf8'));
+
 function getMenu(req, res) {
-  var obj = JSON.parse(_fs2.default.readFileSync(CONSTANT_FILE, 'utf8'));
-  res.json(obj);
+  res.json(constant);
+}
+
+function getCollection(req, res) {
+  res.json(collection);
+}
+
+function getAllProduct(req, res) {
+  res.json(product);
+}
+
+function getProduct(req, res) {
+  var data = {};
+  for (var i = 0; i < product.products.length; i++) {
+    var prod = product.products[i];
+    if (req.params.id == prod.id) {
+      data = prod;
+      break;
+    }
+  }
+
+  res.json(data);
 }
 //# sourceMappingURL=constant.controller.js.map
--- server/api/constant/index.js
+++ server/api/constant/index.js
@@ -6,6 +6,9 @@ var controller = require('./constant.controller');
 var router = express.Router();
 
 router.get('/menu', controller.getMenu);
+router.get('/collection', controller.getCollection);
+router.get('/product', controller.getAllProduct);
+router.get('/product/:id', controller.getProduct);
 
 module.exports = router;
 //# sourceMappingURL=index.js.map
