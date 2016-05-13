dca5b745daec4a437aecbc6fa22840d253e0d308
Cuong Nguyen <cuong.nguyen@niteco.se>
2016-05-13 22:58:38 +0700

    update

--- server/app.js
+++ server/app.js
@@ -28,6 +28,7 @@ var server = _http2.default.createServer(app);
 require('./config/express').default(app);
 require('./routes').default(app);
 
+/*
 var mongoose = require('mongoose');
 var mongoUri = process.env.mongoURI || _environment2.default.mongoURI;
 // Connect mongo
@@ -38,7 +39,7 @@ db.on('error', console.error.bind(console, 'DB connection error: ' + mongoUri));
 db.once('open', function () {
   console.log('DB connection success! ' + mongoUri);
 });
-
+*/
 // Start server
 function startServer() {
   app.angularFullstack = server.listen(_environment2.default.port, _environment2.default.ip, function () {
